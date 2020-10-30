6ec4552de40c90c85bfa9188e83c5e03dbb2c86b
SvSergeyev <sergeslava@yandex.ru>
2020-10-30 14:37:43 +0300

    employee comparator - done

--- src/Homework/Lesson15/Employee.java
+++ src/Homework/Lesson15/Employee.java
@@ -5,16 +5,40 @@ package Homework.Lesson15;
 // имени и зарплате
 // имени, зарплате, возрасту и компании
 
-import java.util.ArrayList;
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
-public class Employee {
+import java.util.*;
+
+public class Employee implements Comparable<Employee> {
     private String name;
     private String company;
     private int salary;
     private int age;
 
     // TODO: конструктор, геттеры и сеттеры
+
+    public Employee(String name, String company, int salary, int age) {
+        Objects.requireNonNull(name, "name is null");
+        Objects.requireNonNull(company, "company is null");
+        if (salary < 1000 || salary > 5000) throw new IllegalArgumentException("salary < 1000 or salary > 5000");
+        if (age < 21 || age > 60) throw new IllegalArgumentException("age < 21 or age > 60");
+        this.name = name;
+        this.company = company;
+        this.salary = salary;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getCompany() {
+        return company;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public int getAge() {
+        return age;
+    }
 
     public static List<Employee> employeeGenerator(int num) {
         // метод для создания списка объектов класса Employee
@@ -25,8 +49,66 @@ public class Employee {
 
         // добавление num объектов Employee в список (employees)
         for (int i = 0; i < num; i++) {
-            employees.add(new Employee(   )); // TODO: объекты создавать с рандомными значениями. Возраст от 21 до 60 и не забудьте про зп
+            // TODO: объекты создавать со случайными значениями. Возраст от 21 до 60 и не забудьте про зп
+            Random r = new Random();
+            employees.add(new Employee(names[r.nextInt(names.length)],
+                    companies[r.nextInt(companies.length)],
+                    r.nextInt(5001) + 1000,
+                    r.nextInt(61) + 21));
         }
         return employees;
+    }
+
+
+    public static List<Employee> sortingByName(List<Employee> employees) {
+        Comparator<Employee> employeeComparator = new NameComparator();
+        employees.sort(employeeComparator);
+        return employees;
+    }
+
+    public static List<Employee> sortingByNameAndSalary(List<Employee> employees) {
+        Comparator<Employee> employeeComparator = new NameComparator().thenComparing(new SalaryComparator());
+        employees.sort(employeeComparator);
+        return employees;
+    }
+
+    public static List<Employee> sortingByAllParameters(List<Employee> employees) {
+        Comparator<Employee> employeeComparator = new NameComparator()
+                .thenComparing(new SalaryComparator())
+                .thenComparing(new AgeComparator())
+                .thenComparing(new CompanyComparator());
+        employees.sort(employeeComparator);
+        return employees;
+    }
+
+
+    @Override
+    public int compareTo(@NotNull Employee o) {
+        return Integer.compare(this.getAge(), o.getAge());
+    }
+}
+
+class NameComparator implements Comparator<Employee> {
+    @Override
+    public int compare(@NotNull Employee o1, Employee o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
+class CompanyComparator implements Comparator<Employee> {
+    @Override
+    public int compare(@NotNull Employee o1, Employee o2) {
+        return o1.getCompany().compareTo(o2.getCompany());
+    }
+}
+class SalaryComparator implements Comparator<Employee> {
+    @Override
+    public int compare(@NotNull Employee o1, Employee o2) {
+        return Integer.compare(o1.getSalary(), o2.getSalary());
+    }
+}
+class AgeComparator implements Comparator<Employee> {
+    @Override
+    public int compare(@NotNull Employee o1, Employee o2) {
+        return Integer.compare(o1.getAge(), o2.getAge());
     }
 }
