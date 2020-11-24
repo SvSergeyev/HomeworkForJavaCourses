41fa9ff3fe123861a88021ca6b3cdd6bf8073134
SvSergeyev <sergeyevsvyatoslav@yandex.ru>
2020-11-24 11:05:12 +0300

    L23-Tasks added

---
+++ src/Homework/lesson23/task/TopTenWords.java
@@ -0,0 +1,23 @@
+package Homework.lesson23.task;
+
+
+public class TopTenWords {
+    public static void main(String[] args) {
+        // создать Map<String, Long> map
+        // String - слово
+        // Long - частота встречаемости
+        // в мапу должны войти только первые 10 частоте встречаемости слов
+
+        // содержимое файла может быть любым
+        // Map<String, Long> map = Files.lines(Paths.get("sources/task23.txt")) // читаем из файла в stream
+        // методы вызывать по цепочке,
+        // цепочку не разрывать (пока не получите результирующую мапу)
+
+        // System.out.println(map);
+
+    }
+}
+
+
+
+
---
+++ src/Homework/lesson23/task/tansactions/Account.java
@@ -0,0 +1,13 @@
+package Homework.lesson23.task.tansactions;
+
+public class Account {
+    private String number;
+    private long balance;
+
+    public Account(String number, long balance) {
+        this.number = number;
+        this.balance = balance;
+    }
+
+    // TODO: добавить геттеры и сеттеры
+}
