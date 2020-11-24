e2c3c3c3efdec972bb61311c23bad525da538bcf
SvSergeyev <sergeslava@yandex.ru>
2020-11-24 23:56:36 +0300

    L23-T1(pupils) - in progress...

--- src/Homework/lesson23/task/pupils/Pupil.java
+++ src/Homework/lesson23/task/pupils/Pupil.java
@@ -1,6 +1,8 @@
 package Homework.lesson23.task.pupils;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
 
 public class Pupil {
     enum Gender {
@@ -11,6 +13,49 @@ public class Pupil {
     private Gender gender;
     private LocalDate birth;
 
-    // TODO: добавить все необходимые методы
 
+    public Pupil(int number, String name, Gender gender, LocalDate birth) {
+        this.number = number;
+        this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public int getAge() {
+        return (int) ChronoUnit.YEARS.between(this.birth, LocalDate.now());
+    }
+
+    @Override
+    public String toString() {
+        return "Pupil{" +
+                "number=" + number +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                ", birth=" + birth +
+                '}';
+    }
+
+    public static LocalDate getRandomBirth() {
+        Random random = new Random();
+        int start = (int) LocalDate.of(1990, 1, 1).toEpochDay();
+        int end = (int) LocalDate.of(2015, 1, 1).toEpochDay();
+        long randomDay = start + random.nextInt(end - start);
+        return LocalDate.ofEpochDay(randomDay);
+    }
 }
--- src/Homework/lesson23/task/pupils/PupilTask.java
+++ src/Homework/lesson23/task/pupils/PupilTask.java
@@ -1,20 +1,49 @@
 package Homework.lesson23.task.pupils;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class PupilTask {
     public static void main(String[] args) {
         // обращение к enum Gender через имя класса - Pupil.Gender
+        ArrayList<Pupil> pupils = new ArrayList<>();
+        pupils.add(new Pupil(100_000, "Sergey", Pupil.Gender.MALE, Pupil.getRandomBirth()));
+        pupils.add(new Pupil(100_001, "Elena", Pupil.Gender.FEMALE, Pupil.getRandomBirth()));
+        pupils.add(new Pupil(100_002, "Anna", Pupil.Gender.FEMALE, Pupil.getRandomBirth()));
+        pupils.add(new Pupil(100_003, "Igor", Pupil.Gender.MALE, Pupil.getRandomBirth()));
+        pupils.add(new Pupil(100_004, "Ivan", Pupil.Gender.MALE, Pupil.getRandomBirth()));
+        pupils.add(new Pupil(100_005, "Alexander", Pupil.Gender.MALE, Pupil.getRandomBirth()));
+        pupils.add(new Pupil(100_006, "Irina", Pupil.Gender.FEMALE, Pupil.getRandomBirth()));
 
         // Используя Stream API:
 
-        // 1. Разделить учеников на две группы: мальчиков и девочек. Результат: Map<Pupil.Gender, ArrayList<Pupil>>
+        // 1. Разделить учеников на две группы: мальчиков и девочек.
+        // Результат: Map<Pupil.Gender, ArrayList<Pupil>>
+        Map<Pupil.Gender, ArrayList<Pupil>> byGender = pupils.stream()
+                .collect(Collectors.groupingBy(Pupil::getGender,
+                        Collectors.toCollection(ArrayList::new)));
+        System.out.println(byGender);
 
         // 2. Найти средний возраст учеников
+        double averageAge = pupils.stream()
+                .mapToDouble(Pupil::getAge).average().orElseGet(null);
+        System.out.printf("%.1f", averageAge);
+        System.out.println();
 
         // 3. Найти самого младшего ученика
+        Pupil youngestPupil = pupils.stream()
+                .min(Comparator.comparing(Pupil::getAge)).orElseGet(null);
+        System.out.println(youngestPupil);
 
         // 4. Найти самого старшего ученика
+        Pupil oldestPupil = pupils.stream()
+                .max(Comparator.comparing(Pupil::getAge)).orElseGet(null);
+        System.out.println(oldestPupil);
 
         // 5. Собрать учеников в группы по году рождения
+
 
         // 6. Убрать учеников с одинаковыми именами, имена и дату рождения оставшихся вывести в консоль
 
