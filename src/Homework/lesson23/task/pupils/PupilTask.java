6dd0e357dc9291265c11f3964950726378384490
SvSergeyev <sergeslava@yandex.ru>
2020-11-26 00:01:06 +0300

    L23-T1(pupils) - in progress...

--- src/Homework/lesson23/task/pupils/PupilTask.java
+++ src/Homework/lesson23/task/pupils/PupilTask.java
@@ -1,8 +1,7 @@
 package Homework.lesson23.task.pupils;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PupilTask {
@@ -10,12 +9,14 @@ public class PupilTask {
         // обращение к enum Gender через имя класса - Pupil.Gender
         ArrayList<Pupil> pupils = new ArrayList<>();
         pupils.add(new Pupil(100_000, "Sergey", Pupil.Gender.MALE, Pupil.getRandomBirth()));
+        pupils.add(new Pupil(100_008, "Sergey", Pupil.Gender.MALE, Pupil.getRandomBirth()));
         pupils.add(new Pupil(100_001, "Elena", Pupil.Gender.FEMALE, Pupil.getRandomBirth()));
         pupils.add(new Pupil(100_002, "Anna", Pupil.Gender.FEMALE, Pupil.getRandomBirth()));
         pupils.add(new Pupil(100_003, "Igor", Pupil.Gender.MALE, Pupil.getRandomBirth()));
         pupils.add(new Pupil(100_004, "Ivan", Pupil.Gender.MALE, Pupil.getRandomBirth()));
         pupils.add(new Pupil(100_005, "Alexander", Pupil.Gender.MALE, Pupil.getRandomBirth()));
         pupils.add(new Pupil(100_006, "Irina", Pupil.Gender.FEMALE, Pupil.getRandomBirth()));
+        pupils.add(new Pupil(100_007, "Irina", Pupil.Gender.FEMALE, Pupil.getRandomBirth()));
 
         // Используя Stream API:
 
@@ -42,13 +43,27 @@ public class PupilTask {
         System.out.println(oldestPupil);
 
         // 5. Собрать учеников в группы по году рождения
-
+        Map<Integer, List<Pupil>> byYearOfBirth = pupils.stream()
+                .collect(Collectors.groupingBy(year -> year.getBirth().getYear()));
+        System.out.println(byYearOfBirth);
 
         // 6. Убрать учеников с одинаковыми именами, имена и дату рождения оставшихся вывести в консоль
+        ArrayList<Pupil> uniqueNames = pupils.stream()
+                .collect(Collectors.collectingAndThen(Collectors.toCollection(
+                        () -> new TreeSet<>(Comparator.comparing(Pupil::getName))),
+                        ArrayList::new));
+        System.out.println(uniqueNames);
 
         // 7. Отсортировать по полу, потом по дате рождения, потом по имени (в обратном порядке), собрать в список (List)
+        List<Pupil> sorted = pupils.stream()
+                .sorted(Comparator.comparing(Pupil::getGender)
+                        .thenComparing(Pupil::getBirth)
+                        .thenComparing(Pupil::getName))
+                .collect(Collectors.toList());
+        System.out.println("last: " + sorted);
 
         // 8. Вывести в консоль всех учеников в возрасте от N до M лет
+
 
         // 9. Собрать в список всех учеников с именем=someName
 
